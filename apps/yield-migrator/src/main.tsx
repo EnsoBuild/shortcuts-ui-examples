e12d0659095e0074e944990baaf6bb1b30ad33da
Dzmitry <zhadkov@gmail.com>
2025-02-11 20:07:03 +0100

    add router to support base path

--- apps/yield-migrator/src/main.tsx
+++ apps/yield-migrator/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import "@rainbow-me/rainbowkit/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
