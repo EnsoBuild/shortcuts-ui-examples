1095b89318b5c7a69bfb04cf93d2d91c8f11e195
Dzmitry <zhadkov@gmail.com>
2025-02-12 16:30:11 +0100

    dark mode for rainbowkit

--- apps/yield-migrator/src/App.tsx
+++ apps/yield-migrator/src/App.tsx
@@ -1,8 +1,10 @@
-import Home from "@/components/Home";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useEffect } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { setApiKey } from "@/service/enso";
 import Providers from "@/Providers";
+import Home from "@/components/Home";
+import { ColorModeButton } from "@/components/ui/color-mode";
+import { Provider } from "@/components/ui/provider";
 
 import logoUrl from "./logo_black_white.png";
 
@@ -10,22 +12,35 @@ function App() {
   useEffect(() => {
     setApiKey(import.meta.env.VITE_ENSO_API_KEY);
   }, []);
+
   return (
     <div style={{ width: "100%", height: "100%" }}>
-      <Providers>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-            margin: "5px",
-          }}
-        >
-          <img src={logoUrl} alt={"Enso"} style={{ height: "50px" }} />
+      <Provider>
+        <Providers>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              margin: "5px",
+            }}
+          >
+            <img src={logoUrl} alt={"Enso"} style={{ height: "50px" }} />
 
-          <ConnectButton />
-        </div>
-        <Home />
-      </Providers>
+            <div
+              style={{
+                display: "flex",
+                gap: "5px",
+                alignItems: "center",
+              }}
+            >
+              <ColorModeButton />
+
+              <ConnectButton />
+            </div>
+          </div>
+          <Home />
+        </Providers>
+      </Provider>
     </div>
   );
 }
--- apps/yield-migrator/src/Providers.tsx
+++ apps/yield-migrator/src/Providers.tsx
@@ -1,8 +1,13 @@
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  darkTheme,
+  getDefaultConfig,
+  lightTheme,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
 import { arbitrum, base, mainnet } from "viem/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Provider } from "@/components/ui/provider";
+import { useColorMode } from "@/components/ui/color-mode";
 
 const config = getDefaultConfig({
   appName: "My RainbowKit App",
@@ -14,11 +19,17 @@ const config = getDefaultConfig({
 const queryClient = new QueryClient();
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const { colorMode } = useColorMode();
+
+  console.log(colorMode);
+
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <Provider>{children} </Provider>
+        <RainbowKitProvider
+          theme={colorMode === "dark" ? darkTheme() : lightTheme()}
+        >
+          {children}
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
